70a9137fa82409a0c2eba4360a448f4738247299
Tobias Simetsreiter <dasimmet@gmail.com>
2024-08-09 22:05:00 +0200

    reset zon

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 const libxml2 = @import("libxml2.zig");
-const zlib = @import("test/zig-zlib/zlib.zig");
+// const zlib = @import("zlib").zlib;
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -27,13 +27,13 @@ pub fn build(b: *std.Build) !void {
     // const z = zlib.create(b, target, optimize);
     // z.link(xml2_with_libs.step, .{});
 
-    const static_binding_test = b.addTest(.{
-        .root_source_file = b.path("test/basic.zig"),
-        .optimize = optimize,
-    });
-    xml2.link(static_binding_test);
+    // // const static_binding_test = b.addTest(.{
+    // //     .root_source_file = b.path("test/basic.zig"),
+    // //     .optimize = optimize,
+    // // });
+    // xml2.link(static_binding_test);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&xml2_with_libs.step.step);
-    test_step.dependOn(&static_binding_test.step);
+    // test_step.dependOn(&static_binding_test.step);
 }
